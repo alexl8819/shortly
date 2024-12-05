b9c3343327431d47be0b8d1e31a0d8d9f64a4e98
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2024-12-05 01:28:16 -0800

    adds in better login experience by handling token refreshes via state changes

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -4,3 +4,22 @@ export const supabaseClient = createClient(
   import.meta.env.SUPABASE_URL,
   import.meta.env.SUPABASE_ANON_KEY
 );
+
+supabaseClient.auth.onAuthStateChange((event) => {
+  if (event === 'SIGNED_IN') {
+    // handle sign in event
+    supabaseClient.auth.startAutoRefresh();
+  } else if (event === 'SIGNED_OUT') {
+    // handle sign out event
+    supabaseClient.auth.stopAutoRefresh();
+  } else if (event === 'TOKEN_REFRESHED') {
+    // handle token refreshed event
+    console.log('token refreshed');
+  } /*else if (event === 'INITIAL_SESSION') {
+    // handle initial session
+  } else if (event === 'PASSWORD_RECOVERY') {
+    // handle password recovery event
+  } else if (event === 'USER_UPDATED') {
+    // handle user updated event
+  }*/
+});
