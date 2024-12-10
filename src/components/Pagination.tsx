e02cd078a31bca1d7f36cd1262dd85c2c8e09212
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2024-12-09 20:28:20 -0800

    temporarily adds flex-wrap to large pagination lists generated

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -9,10 +9,13 @@ interface PaginationProps {
     nextPage: Function
 }
 
+const VISIBLE_LIMIT = 25;
+
 export const Pagination: FC<PaginationProps> = ({ curPage, nextPage, total, limit = 10 }) => {
     const pages = new Array(Math.ceil(total / limit)).fill(1);
+
     return (
-        <ol className='mt-4 list-none flex flex-row justify-center items-center space-x-6 w-full'>
+        <ol className='mt-4 list-none flex flex-row justify-center items-center space-x-6 flex-wrap overflow-x-hidden w-full'>
             {
                 pages && pages.length > 1 ? pages.map((_, index) => (
                     <li key={index} className={`${curPage === index ? 'font-bold' : ''} disabled:cursor-not-allowed`}>
