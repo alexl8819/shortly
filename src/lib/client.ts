361d24d5e28d371080c840483f88d300db6839aa
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2024-12-12 16:41:25 -0800

    fix: removes onAuthStateChange handler to explicitly start/stop auto refresh, enables auto refresh on supabase client creation

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,24 +2,10 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabaseClient = createClient(
   import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_ANON_KEY
+  import.meta.env.SUPABASE_ANON_KEY,
+  { 
+    auth: {
+      autoRefreshToken: true
+    } 
+  }
 );
-
-supabaseClient.auth.onAuthStateChange((event) => {
-  if (event === 'SIGNED_IN') {
-    // handle sign in event
-    supabaseClient.auth.startAutoRefresh();
-  } else if (event === 'SIGNED_OUT') {
-    // handle sign out event
-    supabaseClient.auth.stopAutoRefresh();
-  } /*else if (event === 'TOKEN_REFRESHED') {
-    // handle token refreshed event
-    console.log('token refreshed');
-  } else if (event === 'INITIAL_SESSION') {
-    // handle initial session
-  } else if (event === 'PASSWORD_RECOVERY') {
-    // handle password recovery event
-  } else if (event === 'USER_UPDATED') {
-    // handle user updated event
-  }*/
-});
