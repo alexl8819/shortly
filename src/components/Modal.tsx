99962130ee3aed25e6b13a4bcaf563240590795e
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2024-12-19 20:30:23 -0800

    fix: fixes broken active modal background color, adds border and more shadow to inner modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -16,9 +16,9 @@ export const ConfirmModal: FC<ConfirmModalProps> = ({ title, content }) => {
     }
 
     return (
-        <div className='fixed inset-0 bg-gray-800 bg-opacity-50 z-50'>
+        <div className='fixed inset-0 bg-gray bg-opacity-50 z-50'>
             <div className="flex justify-center items-center min-h-screen">
-                <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
+                <div className="bg-white border border-gray p-8 rounded-lg shadow-2xl max-w-sm w-full">
                     <h2 className="text-xl font-bold mb-4">{ title } </h2>
                     <p className="mb-4">{ content }: <strong className='font-bold'>{ selectedItem }</strong></p>
                     <div className="flex justify-end items-center">
