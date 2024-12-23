1cd0f8ed4b47fce134cabf5673d573a11e952b9a
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2024-12-23 15:54:52 -0800

    change card avatar to load eagerly instead of lazy

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -29,7 +29,7 @@ export const Card: FC<CardProps> = ({ id, title, icon, alt, description }) => {
 		<li key={id} className={`lg:mb-0 mb-20 lg:w-2/6 lg:px-[0.938rem] lg:${id === 1 ? 'mt-6' : (id === 2 ? 'mt-8' : 'mt-0')}`}>
 			<article className='flex flex-col lg:justify-start justify-center xl:items-start items-center xl:text-left text-center bg-white'>
 				<div className='xl:ml-6 flex justify-center items-center rounded-full bg-dark-violet w-[5.5rem] h-[5.5rem] lg:-mt-12 -mt-10'>
-					{ iconImg ? <img className='w-[2.5rem] h-[2.5rem]' src={iconImg} alt={alt} loading='lazy' /> : null }
+					{ iconImg ? <img className='w-[2.5rem] h-[2.5rem]' src={iconImg} alt={alt} loading='eager' /> : null }
 				</div>
 				<div className='p-7'>
 					<h3 className='mt-[2.063rem] font-bold text-[1.375rem] text-very-dark-blue'>{ title }</h3>
