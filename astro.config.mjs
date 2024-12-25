32cb99ebaf559407e6b4ca2792b5b252bda472f0
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2024-12-24 20:03:31 -0800

    enable prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,6 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
+  prefetch: true
 });
