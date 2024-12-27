846ff3a33e2ebc7fe8197dc6082081538e164cd5
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2024-12-26 23:29:15 -0800

    change submit button in ShortenerWidget to use gray background when disabled

--- src/components/Shortener.tsx
+++ src/components/Shortener.tsx
@@ -109,7 +109,7 @@ export const ShortenerWidget: FC<ShortenerProps> = ({ isLoggedIn }) => {
                 </TextField>
                 <Button 
                     type='submit' 
-                    className='lg:mt-0 mt-4 lg:ml-6 lg:py-0 py-3 lg:px-12 px-6 rounded-lg hover:bg-light-cyan bg-cyan text-white lg:w-auto w-full lg:max-h-[3.15rem] text-center cursor-pointer disabled:cursor-not-allowed'
+                    className='lg:mt-0 mt-4 lg:ml-6 lg:py-0 py-3 lg:px-12 px-6 rounded-lg hover:bg-light-cyan bg-cyan text-white lg:w-auto w-full lg:max-h-[3.15rem] text-center cursor-pointer disabled:bg-gray disabled:cursor-not-allowed'
                     isDisabled={isLoading}
                 >
                     Shorten
