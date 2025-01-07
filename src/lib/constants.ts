c44d69cfdc0ca67e2e1e7ecfc21068f033ee5ab2
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2025-01-07 15:23:21 -0800

    move client ip headers to constants  and adds new headers to evaluate when requesting client ip

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -22,3 +22,6 @@ export const DATETIME_PICKER_OPTIONS = {
 }
 export const MINIMUM_MINUTE_DIFF = 5;
 export const MINIMUM_DATETIME = Date.now() + (60 * 1000 * (MINIMUM_MINUTE_DIFF + 1));
+export const CLIENT_IP_HEADERS = (headers: Headers) => headers.get('x-client-ip') || headers.get('x-forwarded-for')?.split(',')[0] || headers.get('cf-connecting-ip')
+|| headers.get('fastly-client-ip') || headers.get('true-client-ip') || headers.get('x-real-ip') || headers.get('x-cluster-client-ip') || headers.get('appengine-user-ip')
+|| headers.get('x-forwarded') || headers.get('forwarded-for') || headers.get('cf-pseudo-ipv4');
