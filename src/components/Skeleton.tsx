4aef8a46c5ff12d0758b8b6bbcd91117d09fc9a1
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2025-06-20 17:02:36 -0700

    add motion safe pref to skeleton animations

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -13,7 +13,7 @@ export const TableSkeleton: FC<TableSkeletonProps> = ({ cols, rows }) => {
                     {
                         Array.from({ length: cols }).map((_, index) => (
                             <th key={index}>
-                                <div className='bg-grayish-violet w-28 h-10 mb-4 mx-auto animate-pulse'></div>
+                                <div className='bg-grayish-violet w-28 h-10 mb-4 mx-auto motion-safe:animate-pulse'></div>
                             </th>
                         ))
                     }
@@ -26,7 +26,7 @@ export const TableSkeleton: FC<TableSkeletonProps> = ({ cols, rows }) => {
                             {
                                 Array.from({ length: cols }).map((_, i) => (
                                     <td key={i}>
-                                        <div className='bg-grayish-violet h-10 w-20 my-6 mx-auto animate-pulse'></div>
+                                        <div className='bg-grayish-violet h-10 w-20 my-6 mx-auto motion-safe:animate-pulse'></div>
                                     </td>
                                 ))
                             }
@@ -41,8 +41,8 @@ export const TableSkeleton: FC<TableSkeletonProps> = ({ cols, rows }) => {
 export const WidgetSkeleton = () => {
     return (
         <div className='flex flex-row w-full'>
-            <div className='bg-grayish-violet rounded-lg h-12 w-2/3 animate-pulse'></div>
-            <div className='bg-grayish-violet rounded-lg h-12 w-1/3 ml-3 animate-pulse'></div>
+            <div className='bg-grayish-violet rounded-lg h-12 w-2/3 motion-safe:animate-pulse'></div>
+            <div className='bg-grayish-violet rounded-lg h-12 w-1/3 ml-3 motion-safe:animate-pulse'></div>
         </div>
     );
 }
@@ -50,7 +50,7 @@ export const WidgetSkeleton = () => {
 export const ChartSkeleton = () => {
     return (
         <div className='w-full'>
-            <div className='bg-grayish-violet rounded-md h-96 w-full animate-pulse'></div>
+            <div className='bg-grayish-violet rounded-md h-96 w-full motion-safe:animate-pulse'></div>
         </div>
     );
 }
@@ -65,7 +65,7 @@ export const ListSkeleton: FC<ListSkeletonProps> = ({ rows }) => {
             {
                 Array.from({ length: rows }).map((_, index) => (
                     <li key={index} className='mb-6 w-full'>
-                        <div className='bg-grayish-violet rounded-md h-32 w-full animate-pulse'></div>
+                        <div className='bg-grayish-violet rounded-md h-32 w-full motion-safe:animate-pulse'></div>
                     </li>
                 ))
             }
@@ -75,6 +75,6 @@ export const ListSkeleton: FC<ListSkeletonProps> = ({ rows }) => {
 
 export const GenericSkeletonItem = () => {
     return (
-        <div className='bg-grayish-violet rounded-lg h-12 w-full animate-pulse'></div>
+        <div className='bg-grayish-violet rounded-lg h-12 w-full motion-safe:animate-pulse'></div>
     )
 }
