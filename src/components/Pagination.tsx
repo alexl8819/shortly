fbfc45344a2395d6a82ebc63302f68bc62b718b7
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2025-06-20 17:23:28 -0700

    temporarily adds new underlying pagination module to improve ability to paginate

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,34 +1,135 @@
+import { usePagination } from 'pagination-react-js';
 import { Button } from 'react-aria-components';
-import { type FC } from 'react';
+import { type FC, type PropsWithChildren } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PropTypes from 'prop-types';
+import { faAngleLeft, faAngleRight, faAnglesLeft, faAnglesRight } from "@fortawesome/free-solid-svg-icons";
 
 interface PaginationProps {
     total: number,
-    limit?: number,
+    limit: number,
     curPage: number,
     nextPage: Function
 }
 
-const VISIBLE_LIMIT = 25;
+interface PaginationItemProps extends PropsWithChildren {
+    label: string
+    active?: boolean
+    onPress: () => void
+}
 
-export const Pagination: FC<PaginationProps> = ({ curPage, nextPage, total, limit = 10 }) => {
-    const pages = new Array(Math.ceil(total / limit)).fill(1);
+const PaginationItem: FC<PaginationItemProps> = ({ children, label, active, onPress }) => {
+    return (
+        <li>
+            <Button
+                className={`${active ? 'font-bold' : 'font-medium'}`}
+                onPress={onPress}
+                aria-current={active ?? "page"}
+                aria-label={label}
+            >
+                { children }
+            </Button>
+        </li>
+    );
+}
+
+export const Pagination: FC<PaginationProps> = ({ curPage, nextPage, total, limit }) => {
+    const { pageNumbers, setActivePage } = usePagination({
+        activePage: curPage,
+        recordsPerPage: limit,
+        totalRecordsLength: total,
+        offset: 1,
+        navCustomPageSteps: { prev: 2, next: 2 },
+        permanentFirstNumber: true,
+        permanentLastNumber: true,
+    });
 
     return (
-        <ol className='mt-4 list-none flex flex-row justify-center items-center space-x-6 flex-wrap overflow-x-hidden w-full'>
-            {
-                pages && pages.length > 1 ? pages.map((_, index) => (
-                    <li key={index} className={`${curPage === index ? 'font-bold rounded-md bg-cyan text-white' : ''} py-1.5 px-3 disabled:cursor-not-allowed`}>
-                        <Button 
-                            onPress={(_) => nextPage(index)}
-                            isDisabled={curPage === index}
+        <nav>
+            <ol className='mt-4 list-none flex flex-row justify-center items-center space-x-6 flex-wrap overflow-x-hidden w-full'>
+                <PaginationItem 
+                    label={`Move to first page - ${pageNumbers.firstPage}`} 
+                    onPress={() => {
+                        setActivePage(pageNumbers.firstPage);
+                        nextPage(pageNumbers.firstPage);
+                    }
+                }>
+                    <FontAwesomeIcon icon={faAnglesLeft} style={{color:"grey"}} />
+                </PaginationItem>
+
+                <PaginationItem 
+                    label={`Move to previous page - ${pageNumbers.previousPage}`}
+                    onPress={() => {
+                        if (pageNumbers.previousPage) {
+                            setActivePage(pageNumbers.previousPage);
+                            nextPage(pageNumbers.previousPage);
+                        }
+                    }
+                }>
+                    <FontAwesomeIcon icon={faAngleLeft} style={{color:"grey"}} />
+                </PaginationItem>
+
+                <PaginationItem
+                    label={`Move to first page - ${pageNumbers.firstPage}`}
+                    active={pageNumbers.firstPage === pageNumbers.activePage}
+                    onPress={() => {
+                        setActivePage(pageNumbers.firstPage);
+                        nextPage(pageNumbers.firstPage);
+                    }}
+                >
+                    { pageNumbers.firstPage }
+                </PaginationItem>
+
+                {
+                    pageNumbers.navigation.map((navigationNumber) => {
+                        return navigationNumber === pageNumbers.firstPage || navigationNumber === pageNumbers.lastPage ? null : (
+                            <PaginationItem
+                                label={`Move to page - ${navigationNumber}`}
+                                key={navigationNumber}
+                                active={navigationNumber === pageNumbers.activePage}
+                                onPress={() => {
+                                    setActivePage(navigationNumber);
+                                    nextPage(navigationNumber);
+                                }}
+                            >
+                                { navigationNumber }
+                            </PaginationItem>
+                        );
+                    })
+                }
+
+                {
+                    pageNumbers.firstPage !== pageNumbers.lastPage && (
+                        <PaginationItem
+                            label={`Move to last page - ${pageNumbers.lastPage}`}
+                            active={pageNumbers.lastPage === pageNumbers.activePage}
+                            onPress={() => {
+                                setActivePage(pageNumbers.lastPage);
+                                nextPage(pageNumbers.lastPage);
+                            }}
                         >
-                            { (index + 1) }
-                        </Button>
-                    </li>
-                )) : null
-            }
-        </ol>
+                            { pageNumbers.lastPage}
+                        </PaginationItem>
+                    )
+                }
+
+                <PaginationItem label={`Move to next page - ${pageNumbers.nextPage}`} onPress={() => {
+                    if (pageNumbers.nextPage) {
+                        setActivePage(pageNumbers.nextPage);
+                        nextPage(pageNumbers.nextPage);
+                    }
+                }}>
+                    <FontAwesomeIcon icon={faAngleRight} style={{color:"grey"}} />
+                </PaginationItem>
+
+                <PaginationItem label={`Move to last page - ${pageNumbers.lastPage}`} onPress={() => {
+                    setActivePage(pageNumbers.lastPage);
+                    nextPage(pageNumbers.lastPage);
+                }}>
+                    <FontAwesomeIcon icon={faAnglesRight} style={{color:"grey"}} />
+                </PaginationItem>
+            </ol>
+        </nav>
     );
 }
 
@@ -36,7 +137,7 @@ Pagination.propTypes = {
     curPage: PropTypes.number.isRequired,
     nextPage: PropTypes.func.isRequired,
     total: PropTypes.number.isRequired,
-    limit: PropTypes.number
+    limit: PropTypes.number.isRequired
 };
 
 export default Pagination;
