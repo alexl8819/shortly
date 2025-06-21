a8a76a35068d3a9063de368236cfbd73aee6b097
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2025-06-20 19:22:46 -0700

    enable checkOrigin security feature for csrf protection

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  security: {
+    checkOrigin: true
+  },
   output: 'server',
   adapter: vercel(),
   prefetch: true
